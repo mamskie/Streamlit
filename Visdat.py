ee2073dda6aa8e098a602d3e0873c4243733da6e
MAM <86782580+mamskie@users.noreply.github.com>
2024-02-20 10:25:29 +0700

    Update Visdat.py

--- Visdat.py
+++ Visdat.py
@@ -73,13 +73,20 @@ div[data-testid="metric-container"] > label[data-testid="stMetricLabel"] > div {
 , unsafe_allow_html=True)
 # -----------------------------------------------------------------------------------------------------------------------------------
 if menu_id == 'Home':
-    st.write("""# Visualisasi Data Kepadatan Penduduk Jawa Timur dengan Streamlit""")
-    st.write("""## Aplikasi ini dikembangkan untuk melakukan visualisasi data kepadatan penduduk di Jawa Timur menggunakan Streamlit, sebuah framework Python untuk membuat aplikasi web interaktif dengan mudah. Aplikasi ini memanfaatkan beberapa library tambahan seperti Pandas, NumPy, Hydralit Components, Altair, dan Plotly Express untuk analisis dan visualisasi data. """)
-    st.write("""## Catatan :""")
-    a1,a2,a3 = st.columns(3)
-    a1.metric("Aplikasi ini menggunakan Streamlit dan beberapa library visualisasi data untuk menyajikan informasi dengan cara yang lebih menarik dan mudah dipahami.")
-    a2.metric("Pastikan semua dependensi terinstall dengan benar")
-    a3.metric("Gunakan menu navigasi di bagian atas aplikasi untuk beralih antara berbagai tampilan dan fitur yang telah disediakan")
+st.write("""# Visualisasi Data Kepadatan Penduduk Jawa Timur dengan Streamlit""")
+st.write("""## Aplikasi ini dikembangkan untuk melakukan visualisasi data kepadatan penduduk di Jawa Timur menggunakan Streamlit, sebuah framework Python untuk membuat aplikasi web interaktif dengan mudah. Aplikasi ini memanfaatkan beberapa library tambahan seperti Pandas, NumPy, Hydralit Components, Altair, dan Plotly Express untuk analisis dan visualisasi data. """)
+st.write("""## Catatan :""")
+a1, a2, a3 = st.columns(3)
+
+with a1:
+    st.metric("Aplikasi ini menggunakan Streamlit dan beberapa library visualisasi data untuk menyajikan informasi dengan cara yang lebih menarik dan mudah dipahami.")
+
+with a2:
+    st.metric("Pastikan semua dependensi terinstall dengan benar")
+
+with a3:
+    st.metric("Gunakan menu navigasi di bagian atas aplikasi untuk beralih antara berbagai tampilan dan fitur yang telah disediakan")
+
 # -----------------------------------------------------------------------------------------------------------------------------------
 if menu_id == 'Dataset':
     st.write("""## Dataset Keseluruhan""") 
